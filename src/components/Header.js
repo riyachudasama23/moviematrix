a9b592329e96b3a44458abbeac08fc3fac768a80
Riya <riyachudasama1123@gmail.com>
2024-09-13 22:03:15 +0530

    third commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,18 @@ import React from 'react'
 import logo from '../assets/logo.png'
 
 const Header = () => {
+  const navigation = [
+    {
+      label: "TV Shows",
+      href: 'tv'
+    },
+    {
+      label: "Movies"
+    }
+  ]
   return (
     <div className="fixed top-0 w-full h-16 bg-neutral-600 bg-opacity-75">
-      <div className="container mx-auto px-2"> 
+      <div className="container mx-auto px-2 flex items-center h-full"> 
         <div>
           <img 
             src={logo} 
